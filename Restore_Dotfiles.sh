85e863c159b490926891dd4d92b9e5f824a25261
jandrewmc <jandrewmc@gmail.com>
2020-01-29 13:51:09 -0500

    Update Restore_Dotfiles.sh

--- Restore_Dotfiles.sh
+++ Restore_Dotfiles.sh
@@ -2,5 +2,5 @@
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-cp ~/dev/dotfiles/.vimrc ~/.vimrc
-cp ~/dev/dotfiles.inputrc ~/.inputrc
+cp .vimrc ~/.vimrc
+cp .inputrc ~/.inputrc
